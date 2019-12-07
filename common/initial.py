9f7f7df57d3da754ac017384f4972c026fa09ad4
Onal, Erdogan (IOT DS EU TR CNT SWC 1) <erdogan.onal@siemens.com>
2019-12-07 21:43:28 +0300

    chromedriver.exe check added. Initial check moved into a file.

---
+++ common/initial.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import imageio
+
+import settings
+
+
+def check_ffmpeg_exe():
+    try:
+        imageio.plugins.ffmpeg.get_exe()
+    except imageio.core.fetching.NeedDownloadError:
+        sys.stderr.write("Failed to import video upload library.\n")
+        sys.stderr.write("Downloading the executable. Please wait...\n")
+        imageio.plugins.ffmpeg.download()
+        sys.stderr.write(
+            "\nDownload has been completed. Program should be restarted.\n"
+        )
+        sys.exit("Exiting...")
+
+
+def check_chrome_driver():
+    if settings.MASTER_WITH_GUI and not os.path.isfile(settings.CHROME_DRIVER):
+        sys.stderr.write("Failed to locate chromedriver.\n")
+        sys.stderr.write("Please locate the driver under base path ")
+        sys.stderr.write("or define it's path in settings.py\n")
+        sys.exit("Exiting...")
+
+
+def initial_check():
+    sys.__stdout__.write("Initial check. Please wait...\n")
+    sys.__stdout__.flush()
+
+    check_ffmpeg_exe()
+
+    check_chrome_driver()
+
+    sys.__stdout__.write("Initial check is done. Everything is well\n")
+    sys.__stdout__.flush()
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 
 "This is where the program starts."
-import sys
-
-import imageio
-
+from common.initial import initial_check
 from common.controller import start_app
-
-try:
-    imageio.plugins.ffmpeg.get_exe()
-except imageio.core.fetching.NeedDownloadError:
-    sys.stderr.write("Failed to import video upload library.\n")
-    sys.stderr.write("Downloading the executable. Please wait...\n")
-    imageio.plugins.ffmpeg.download()
-    sys.stderr.write("\nDownload has been completed. Program should be restarted.\n")
-    sys.exit("Exiting...")
 
 
 def main():
     "Starts the program"
+    initial_check()
     start_app()
 
 
